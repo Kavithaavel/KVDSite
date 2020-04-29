4da0fce236075401d0e231e5226e0a117e0a6d81
Kavi <kavithaavel@gmail.com>
2020-04-28 20:52:40 -0500

    R Week3 Functional Components

--- src/components/HotelDirectoryComponent.js
+++ src/components/HotelDirectoryComponent.js
@@ -1,31 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-class HotelDirectory extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+function RenderHotelDirectoryItem({ hotelsite, onClick }) {
+  return (
+    <Card onClick={() => onClick(hotelsite.id)}>
+      <CardImg top src={hotelsite.image} alt={hotelsite.name} />
+      <CardImgOverlay>
+        <CardTitle>{hotelsite.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+}
 
-  render() {
-    const hotelDirectory = this.props.hotelsites.map((hotelsite) => {
-      return (
-        <div key={hotelsite.id} className="col-md-5 m-1">
-          <Card onClick={() => this.props.onClick(hotelsite.id)}>
-            <CardImg top src={hotelsite.image} alt={hotelsite.name} />
-            <CardImgOverlay>
-              <CardTitle>{hotelsite.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
-    });
+function HotelDirectory(props) {
+  const hotelDirectory = props.hotelsites.map((hotelsite) => {
     return (
-      <div className="container">
-        <div className="row">{hotelDirectory}</div>
+      <div key={hotelsite.id} className="col-md-5 m-1">
+        <RenderHotelDirectoryItem
+          hotelsite={hotelsite}
+          onClick={props.onClick}
+        />
       </div>
     );
-  }
+  });
+  return (
+    <div className="container">
+      <div className="row">{hotelDirectory}</div>
+    </div>
+  );
 }
 
 export default HotelDirectory;
--- src/components/HotelsiteInfoComponent.js
+++ src/components/HotelsiteInfoComponent.js
@@ -1,71 +1,63 @@
-import React, { Component } from "react";
-
+import React from "react";
 import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 
-class HotelsiteInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+function RenderComments({ comments }) {
+  if (comments) {
+    return (
+      <div className="col">
+        <Card>
+          <CardBody>
+            <CardTitle>Comments</CardTitle>
+          </CardBody>
+          <CardBody>
+            {comments.map((comment) => (
+              <div key={comment.id}>
+                {comment.text} <br />
+                -- {comment.author},{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comment.date)))}
+              </div>
+            ))}
+          </CardBody>
+        </Card>
+      </div>
+    );
   }
+  return <div />;
+}
 
-  renderComments(comments) {
-    if (comments) {
-      return (
-        <div className="col">
-          <Card>
-            <CardBody>
-              <CardTitle>Comments</CardTitle>
-            </CardBody>
-            <CardBody>
-              {comments.map((comment) => (
-                <div key={comment.id}>
-                  {comment.text} <br />
-                  -- {comment.author},{" "}
-                  {new Intl.DateTimeFormat("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                  }).format(new Date(Date.parse(comment.date)))}
-                </div>
-              ))}
-            </CardBody>
-          </Card>
-        </div>
-      );
-    }
-    return <div />;
+function RenderHotelsite({ hotelsite }) {
+  if (hotelsite) {
+    return (
+      <div className="col m-1">
+        <Card>
+          <CardImg top src={hotelsite.image} alt={hotelsite.name} />
+          <CardBody>
+            <CardTitle>{hotelsite.name}</CardTitle>
+            <CardTitle>{hotelsite.description}</CardTitle>
+          </CardBody>
+        </Card>
+      </div>
+    );
   }
+  return <div />;
+}
 
-  renderHotelsite(hotelsite) {
-    if (hotelsite) {
-      return (
-        <div className="col m-1">
-          <Card>
-            <CardImg top src={hotelsite.image} alt={hotelsite.name} />
-            <CardBody>
-              <CardTitle>{hotelsite.name}</CardTitle>
-              <CardTitle>{hotelsite.description}</CardTitle>
-            </CardBody>
-          </Card>
+function HotelsiteInfo(props) {
+  if (props.hotelsite) {
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderHotelsite hotelsite={props.hotelsite} />
+          <RenderComments comments={props.hotelsite.comments} />
         </div>
-      );
-    }
-    return <div />;
+      </div>
+    );
   }
-
-  render() {
-    if (this.props.hotelsite) {
-      return (
-        <div className="container">
-          <div className="row">
-            {this.renderHotelsite(this.props.hotelsite)}
-            {this.renderComments(this.props.hotelsite.comments)}
-          </div>
-        </div>
-      );
-    }
-    return <div />;
-  }
+  return <div />;
 }
 
 export default HotelsiteInfo;
