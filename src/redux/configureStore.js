8cd60c9f26f422d7c66a0b7ff974afa83bd8dc83
Kavi <kavithaavel@gmail.com>
2020-05-07 23:05:59 -0500

    R W5 Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,19 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { createStore, combineReducers } from "redux";
+//import { Reducer, initialState } from "./reducer";
+import { Hotelsites } from "./hotelsites";
+import { Comments } from "./comments";
+import { Partners } from "./partners";
+import { Promotions } from "./promotions";
 
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, initialState);
+  const store = createStore(
+    combineReducers({
+      hotelsites: Hotelsites,
+      comments: Comments,
+      partners: Partners,
+      promotions: Promotions,
+    })
+  );
 
   return store;
 };
---
+++ src/redux/hotelsites.js
@@ -0,0 +1,8 @@
+import { HOTELSITES } from "../shared/hotelsites";
+
+export const Hotelsites = (state = HOTELSITES, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
